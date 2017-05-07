0741385d1d285d816efb8b5d11e1e1092e62d7c2
YYinDa <yiniski@gmail.com>
2017-05-07 23:16:26 +1200

    stub for ad items for home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core'
 import { NavController } from 'ionic-angular'
+import { ToastController } from 'ionic-angular'
+
 import { Batch } from '../../providers/batch'
+
 
 @Component({
   selector: 'page-home',
@@ -9,17 +12,26 @@ import { Batch } from '../../providers/batch'
 
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private batch: Batch) {
+  constructor(
+    public navCtrl: NavController,
+    public toastCtrl: ToastController,
+    private batch: Batch) { }
+
+  showHelpHint() {
+    let toast = this.toastCtrl.create({
+      message: `let me know if you need help navigating around here.`,
+      duration: 5000,
+    })
+
+    toast.present()
   }
 
   ngOnInit() {
     this.batch.get<any>()
       .then(res => {
-        let b = res
+        let ad = res
       })
-  }
 
-  getItems(e: any) {
-
+    this.showHelpHint()
   }
 }
